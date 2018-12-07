8e2b9083b7ea116ce94bd24b681194ad3259c1fd
freshbrashman <fresh.brash.sarariiman@gmail.com>
2018-12-07 16:32:02 +0900

    ルーティングの修正

--- src/main/react/index.tsx
+++ src/main/react/index.tsx
@@ -3,7 +3,7 @@ import './css/base.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { DragSource } from "react-dnd";
-import { Router, Route } from "react-router";
+import { Router, Route, RouteComponentProps } from "react-router";
 import { Link } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
@@ -12,24 +12,6 @@ import * as _ from "lodash";
 import * as Element from './elements/element'
 import {TaskDialog} from './pageparts/dialogs';
 
-const history = createHistory();
-
-interface AppProps {
-}
-interface AppState {
-}
-class App extends React.Component<AppProps, AppState> {
-  render() {
-    return (
-      <div>
-        <h1>App</h1>
-        <ul>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/messages/10">Message</Link></li>
-        </ul>
-      </div>
-    )
-  }}
 
 interface TaskListBaseProps {
 }
@@ -199,16 +181,45 @@ ReactDOM.render(
 );
 */
 
+
+const history = createHistory();
+
+interface AppProps {
+}
+interface AppState {
+}
+class App extends React.Component<AppProps, AppState> {
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/task">Task</Link></li>
+          <li><Link to="/messages/10">Message</Link></li>
+        </ul>
+      </div>
+    )
+  }
+}
+
 export class About extends React.Component {
   render() {
     return <h3>About</h3>
   }
 }
 
-export class Message extends React.Component {
+interface MessageParams {
+  id: string,
+}
+export class Message extends React.Component<RouteComponentProps<MessageParams>, any> {
   render() {
-//    return <h3>Message {this.props.match.params.id}</h3>
-    return <h3>Message</h3>
+    return (
+      <div>
+        <h3>Message {this.props.match.params.id}</h3>
+        <li><Link to="/">トップへ戻るわよ！</Link></li>
+      </div>
+    )
   }
 }
 
@@ -218,6 +229,7 @@ ReactDOM.render(
     <div>
       <Route exact path="/" component={App} />
       <Route path="/about" component={About} />
+      <Route path="/task" component={TaskListBase} />
       <Route path="/messages/:id" component={Message} />
     </div>
   </Router>,
