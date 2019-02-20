52bcb970a110e8f1cccc0ff8503696868c4aa690
freshbrashman <fresh.brash.sarariiman@gmail.com>
2019-02-20 23:55:43 +0900

    Front側のルーティングのやり方を変更

--- py/urls/front.py
+++ py/urls/front.py
@@ -3,10 +3,13 @@ from flask import Flask, Blueprint, render_template, request, redirect, url_for,
 # Blueprintにて、APIパスのprefix定義
 front = Blueprint('', __name__)
 
-@front.route('/')
-def index():
-    return render_template('index.html')
-
-@front.route('/login/<token>')
-def login(token):
-    return render_template('index.html')
+# トップページをフロント側へルーティング(デコレータ１番め)
+# URL１階層目だけ指定された場合はページを差すのでフロントへルーティング(デコレータ2番目)
+# それ以外は、APIへのアクセスかログインだけとなる。
+# APIへのアクセスはここではルーティングせず、APIのBlueprintの方で実施
+# ログイン処理も受付の頭はフロント側なので、フロントへルーティング(デコレータ3番目)
+@front.route('/', defaults={'path': ''})
+@front.route('/<path>')
+@front.route('/login/<path>')
+def catch_all(path):
+    return render_template("index.html")
