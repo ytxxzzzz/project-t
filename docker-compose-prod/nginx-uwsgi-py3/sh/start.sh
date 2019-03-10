7eb4474e89dc2db0d74464237b553c92610de2f5
freshbrashman <fresh.brash.sarariiman@gmail.com>
2019-03-10 21:10:55 +0900

    ProdのDocker足りないところを色々

--- docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
+++ docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
+export LANG=ja_JP.UTF-8
+
 /etc/init.d/nginx start
 cd /var/www/html/app
+
+# npmのパッケージ再構築
+npm install
+# reactのコンパイル
+npm run build
+
+# pipenvのパッケージ再構築
+pipenv install
+
+# migrationsの再構築
+rm migrations
+FLASK_APP=main.py pipenv run flask db init
+FLASK_APP=main.py pipenv run flask db migrate
+FLASK_APP=main.py pipenv run flask db upgrade
+
 #chmod -R 777 .
 uwsgi --ini uwsgi.ini
