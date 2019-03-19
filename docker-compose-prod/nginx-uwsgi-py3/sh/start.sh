2058cf9022adeaefb37719f0a9b578985516c8e8
freshbrashman <fresh.brash.sarariiman@gmail.com>
2019-03-20 02:02:19 +0900

    Dockerビルドしたディレクトリをそのままコンテナにデプロイするのを廃止
    ビルド時にGitCloneするようにした
    ビルド時にパッケージインストールもやってしまって、動作環境はDockerイメージに固める方針に

--- docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
+++ docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
@@ -2,23 +2,16 @@
 
 export LANG=ja_JP.UTF-8
 
-/etc/init.d/nginx start
 cd /var/www/html/app
-
-# npmのパッケージ再構築
-npm install
-# reactのコンパイル
-npm run build-prod
-
-# pipenvのパッケージ再構築(この環境変数はpipenv仮想環境をプロジェクト直下の".venv"フォルダに作成するおまじない)
-export PIPENV_VENV_IN_PROJECT=1
-pipenv install
 
 # migrationsの再構築
 rm -rf migrations
 FLASK_APP=main.py pipenv run flask db init
 FLASK_APP=main.py pipenv run flask db migrate
 FLASK_APP=main.py pipenv run flask db upgrade
+
+# nginx起動
+/etc/init.d/nginx start
 
 # uwsgiの起動
 export FLASK_ENV=production
