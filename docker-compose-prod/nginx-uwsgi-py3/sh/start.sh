a4f8b8194f4e9eb5fb29697cdc81dbc217991f4d
freshbrashman <fresh.brash.sarariiman@gmail.com>
2019-07-07 15:16:10 +0900

    鍵生成をnginx起動スクリプトの方へ。エラー原因まではわかってないが

--- docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
+++ docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
@@ -5,9 +5,18 @@ export LANG=ja_JP.UTF-8
 # nginx起動
 /etc/init.d/nginx start
 
-cd /var/www/html/app
+# letsencryptでSSL鍵を生成(ドメインの認証をするために、一度nginxの起動が必要)
+git clone https://github.com/letsencrypt/letsencrypt.git /home/letsencrypt
+/home/letsencrypt/letsencrypt-auto --help -y
+/home/letsencrypt/letsencrypt-auto certonly --non-interactive --agree-tos --webroot -w /var/www/html/app/ -d ${SSL_DOMAIN} --email ${SSL_EMAIL}
+
+# SSL鍵の配置
+cp /etc/letsencrypt/live/www.tatatatask.work/fullchain.pem /etc/nginx/fullchain.pem
+cp /etc/letsencrypt/live/www.tatatatask.work/privkey.pem /etc/nginx/privkey.pem
+chmod 400 /etc/nginx/privkey.pem
 
 # migrationsの再構築
+cd /var/www/html/app
 rm -rf migrations
 FLASK_APP=main.py pipenv run flask db init
 FLASK_APP=main.py pipenv run flask db migrate
