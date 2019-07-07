47e013a4c764a1df472b925f8eaea40449b91afe
freshbrashman <fresh.brash.sarariiman@gmail.com>
2019-07-07 20:53:03 +0900

    プロンプトで止まってしまう問題への試行錯誤

--- docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
+++ docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
@@ -7,7 +7,7 @@ export LANG=ja_JP.UTF-8
 
 # letsencryptでSSL鍵を生成(ドメインの認証をするために、一度nginxの起動が必要)
 git clone https://github.com/letsencrypt/letsencrypt.git /home/letsencrypt
-/home/letsencrypt/letsencrypt-auto certonly --non-interactive --agree-tos --webroot -w /var/www/html/app/ -d ${SSL_DOMAIN} --email ${SSL_EMAIL}
+echo yes | /home/letsencrypt/letsencrypt-auto certonly --non-interactive --agree-tos --webroot -w /var/www/html/app/ -d ${SSL_DOMAIN} --email ${SSL_EMAIL}
 
 # SSL鍵の配置
 cp /etc/letsencrypt/live/www.tatatatask.work/fullchain.pem /etc/nginx/fullchain.pem
