e0c8a958bbfc7a26eba1b876069326f469279cf0
freshbrashman <fresh.brash.sarariiman@gmail.com>
2019-07-08 23:44:31 +0900

    letsencrypt関連をDockerfileへ移動した。やはりCMDでやることではないだろうと

--- docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
+++ docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
@@ -8,15 +8,6 @@ export LANG=ja_JP.UTF-8
 # nginx起動
 /etc/init.d/nginx start
 
-# letsencryptでSSL鍵を生成(ドメインの認証をするために、一度nginxの起動が必要)
-git clone https://github.com/letsencrypt/letsencrypt.git /home/letsencrypt
-yes yes | /home/letsencrypt/letsencrypt-auto certonly --non-interactive --agree-tos --webroot -w /var/www/html/app/ -d ${SSL_DOMAIN} --email ${SSL_EMAIL}
-
-# SSL鍵の配置
-cp /etc/letsencrypt/live/www.tatatatask.work/fullchain.pem /etc/nginx/fullchain.pem
-cp /etc/letsencrypt/live/www.tatatatask.work/privkey.pem /etc/nginx/privkey.pem
-chmod 400 /etc/nginx/privkey.pem
-
 /etc/init.d/nginx restart
 
 # migrationsの再構築
