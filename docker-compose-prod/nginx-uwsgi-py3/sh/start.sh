ee004eaa4856cf58408777c4297228071fd56615
freshbrashman <fresh.brash.sarariiman@gmail.com>
2019-07-09 00:02:11 +0900

    dockerビルド中はletsencryptのスタンドアロンサーバでドメインの認証できないっぽい

--- docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
+++ docker-compose-prod/nginx-uwsgi-py3/sh/start.sh
@@ -5,6 +5,14 @@
 
 export LANG=ja_JP.UTF-8
 
+# letsencryptでSSL鍵を生成(ドメインの認証をするために、独自のスタンドアローンなHTTPサーバを内部的に起動する)
+/home/letsencrypt/letsencrypt-auto certonly --standalone --non-interactive --agree-tos -d ${SSL_DOMAIN} --email ${SSL_EMAIL}
+
+# SSL鍵の配置
+cp /etc/letsencrypt/live/${SSL_DOMAIN}/fullchain.pem /etc/nginx/fullchain.pem
+cp /etc/letsencrypt/live/${SSL_DOMAIN}/privkey.pem /etc/nginx/privkey.pem
+chmod 400 /etc/nginx/privkey.pem
+
 # nginx起動
 /etc/init.d/nginx start
 
